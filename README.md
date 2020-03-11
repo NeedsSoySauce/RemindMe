c7d8b9483fedb66f29d3b800f306f6d36620bd15
NeedsSoySauce <feras.albaroudi@gmail.com>
2020-03-12 01:14:14 +1300

    Send emails using SMTP

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # RemindMe
+
+A short script to remind me what bins to take out for rubbish collection.
+
+## Getting started
+
+These steps will help you to deploy to AWS and/or setup a local environment for testing.
+
+### Prerequisites
+
+* pip
+* pipenv
+* awscli
+* An AWS Lambda function to deploy to
+
+### Installing
+
+1. Install dependencies
+
+```bash
+pipenv install
+```
+
+2. Run the project locally
+
+```bash
+pipenv run python ./main.py
+```
+
+### Deployment
+
+1. Install dependencies locally
+
+```
+pip install -t ./package requests bs4 html5lib
+```
+
+**Note:** You may need to use the `--system` flag if you're using debian.
+
+2. Zip the script and it's dependencies into a deployment package
+
+```bash
+cp main.py package
+cd package
+zip -r package.zip *
+```
+
+3. Deploy to AWS
+
+Replace "<func_name>" with the name of your lambda function.
+
+```bash
+aws lambda update-function-code --function-name <func_name> --zip-file fileb://package.zip 
+```
--- main.py
+++ main.py
@@ -1,30 +1,71 @@
+import smtplib
+import os
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 
-url = 'https://www.aucklandcouncil.govt.nz/rubbish-recycling/rubbish-recycling-collections/Pages/collection-day-detail.aspx?an=12342559369'
+SMTP_HOST = os.environ['SMTP_HOST']
+SMTP_PORT = os.environ['SMTP_PORT']
+SMTP_USERNAME = os.environ['SMTP_USERNAME']
+SMPT_PASSWORD = os.environ['SMPT_PASSWORD']
 
-response = requests.get(url)
+SENDER = os.environ['SENDER']
 
-soup = BeautifulSoup(response.text, 'lxml')
-
-# Find parent container for household disposal
-container = soup.find('h3', class_="card-title h2").parent.parent
-
-link_container = container.find('div', class_="links")
-
-# Get string date representation
-date_elem = link_container.find('span', class_="m-r-1")
-is_rubbish_due = link_container.find('span', class_='icon-rubbish') != None
-is_recycling_due = link_container.find('span', class_='icon-recycle') != None
+URL = os.environ['URL']
 
 
-print(date_elem.string)
+def main():
+    response = requests.get(URL)
 
-datetime_format = "%A %d %B"
-collection_date = datetime.strptime(date_elem.string, datetime_format)
-collection_date = collection_date.replace(year=datetime.now().year)
+    soup = BeautifulSoup(response.text, 'html5lib')
 
-print(collection_date)
-print(is_rubbish_due)
-print(is_recycling_due)
+    # Find parent container for household disposal
+    container = soup.find('h3', class_="card-title h2").parent.parent
+    link_container = container.find('div', class_="links")
+
+    # Get date
+    date_elem = link_container.find('span', class_="m-r-1")
+
+    # Get what items are due
+    is_rubbish_due = link_container.find('span', class_='icon-rubbish') != None
+    is_recycling_due = link_container.find('span',
+                                           class_='icon-recycle') != None
+
+    # Parse date into a datetime object
+    datetime_format = "%A %d %B"
+    collection_date = datetime.strptime(date_elem.string, datetime_format)
+    collection_date = collection_date.replace(year=datetime.now().year)
+
+    try:
+        s = smtplib.SMTP(host=SMTP_HOST, port=SMTP_PORT)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        s.login(SMTP_USERNAME, SMPT_PASSWORD)
+
+        msg = MIMEMultipart()
+
+        msg['From'] = SENDER
+        msg['To'] = SENDER
+        msg['Subject'] = "Reminder"
+
+        msg.attach(MIMEText('Hello world!'))
+
+        # send the message via the server set up earlier.
+        s.send_message(msg)
+        s.close()
+    except Exception as e:
+        print("Error: ", e)
+    else:
+        print("Email sent!")
+
+
+def lambda_handler(event, context):
+    main()
+
+
+if __name__ == '__main__':
+    main()
