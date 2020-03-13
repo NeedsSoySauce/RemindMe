405ee45eec914b0fb1dc24d24ae8a3f47a633f33
Feras Albaroudi <feras.albaroudi@gmail.com>
2020-03-13 21:07:47 +1300

    Added deploy script.

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FUNCTION_NAME="$1"
+
+echo "Deploying to '$FUNCTION_NAME'"
+
+# Install dependencies into packages folder
+rm -r ./package
+pip3 install -t ./package requests bs4 html5lib
+
+# Zip dependencies and main script file
+cp main.py package 
+cd package 
+zip -r package.zip *
+
+# Deploy to AWS
+aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://package.zip 
